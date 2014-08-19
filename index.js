7949524c3e0d7d588b1ee71b2759d5d9875c00af
Jason Frame <jason@onehackoranother.com>
2014-08-19 16:15:41 +0100

    tabs -> spaces

--- index.js
+++ index.js
@@ -3,52 +3,52 @@ module.exports = Manager;
 var Collection = require('beeker');
 
 function Manager() {
-	this._collection 	= new Collection();
-	this._activeIndex	= -1;
-	this.events 		= this._collection.events;
+    this._collection    = new Collection();
+    this._activeIndex   = -1;
+    this.events         = this._collection.events;
 }
 
 Manager.prototype.at = function(ix) {
-	return this._collection.at(ix);
+    return this._collection.at(ix);
 }
 
 Manager.prototype.active = function() {
-	if (this._activeIndex < 0) {
-		return null;
-	} else {
-		return this._collection.at(this._activeIndex);
-	}
+    if (this._activeIndex < 0) {
+        return null;
+    } else {
+        return this._collection.at(this._activeIndex);
+    }
 }
 
 Manager.prototype.activate = function(item) {
-	if (typeof item === 'number') {
-		this._setActive(this._collection.at(item), item);	
-	} else {
-		this._setActive(item, this._collection.indexOf(item));
-	}
+    if (typeof item === 'number') {
+        this._setActive(this._collection.at(item), item);   
+    } else {
+        this._setActive(item, this._collection.indexOf(item));
+    }
 }
 
 Manager.prototype.previous = function() {
-	this._cycle(-1);
+    this._cycle(-1);
 }
 
 Manager.prototype.next = function() {
-	this._cycle(1);
+    this._cycle(1);
 }
 
 Manager.prototype.add = function(item) {
-	this._collection.add(item);
-	if (this._collection.length === 1) {
-		this._setActive(this._collection.at(0), 0);
-	}
+    this._collection.add(item);
+    if (this._collection.length === 1) {
+        this._setActive(this._collection.at(0), 0);
+    }
 }
 
 Manager.prototype.remove = function(item) {
-	this._remove(this._collection.indexOf(item));
+    this._remove(this._collection.indexOf(item));
 }
 
 Manager.prototype.removeActive = function() {
-	this._remove(this._activeIndex);
+    this._remove(this._activeIndex);
 }
 
 //
@@ -56,54 +56,54 @@ Manager.prototype.removeActive = function() {
 
 Manager.prototype._setActive = function(item, ix) {
 
-	if (!item || ix < 0) {
-		return;
-	}
+    if (!item || ix < 0) {
+        return;
+    }
 
-	var oldItem = this._activeIndex >= 0
-					? this._collection.at(this._activeIndex)
-					: null;
+    var oldItem = this._activeIndex >= 0
+                    ? this._collection.at(this._activeIndex)
+                    : null;
 
-	this._activeIndex = ix;
+    this._activeIndex = ix;
 
-	this.events.emit('activate', item, oldItem);
+    this.events.emit('activate', item, oldItem);
 
 }
 
 Manager.prototype._remove = function(ix) {
-	if (ix < 0) return;
+    if (ix < 0) return;
 
-	// if (this._collection.length === 0) {
-	// 	// TODO: emit switch
-	// } else if (this._activeIndex >= ix) {
-	// 	this._activeIndex--;
-	// }
+    // if (this._collection.length === 0) {
+    //  // TODO: emit switch
+    // } else if (this._activeIndex >= ix) {
+    //  this._activeIndex--;
+    // }
 
-	// this._collection.removeItemAtIndex(ix);
+    // this._collection.removeItemAtIndex(ix);
 
 
 
-	// if (ix >= )
+    // if (ix >= )
 
-	// if (ix === this._activeIndex) {
-	// 	// TODO: switch
-	// }
+    // if (ix === this._activeIndex) {
+    //  // TODO: switch
+    // }
 }
 
 Manager.prototype._cycle = function(delta) {
 
-	if (this._collection.length === 0) {
-		return;
-	}
-	
-	var ix = this._activeIndex + delta;
+    if (this._collection.length === 0) {
+        return;
+    }
+    
+    var ix = this._activeIndex + delta;
 
-	if (ix < 0) {
-		ix = this._collection.length - 1;
-	} else if (ix >= this._collection.length) {
-		ix = 0;
-	}
+    if (ix < 0) {
+        ix = this._collection.length - 1;
+    } else if (ix >= this._collection.length) {
+        ix = 0;
+    }
 
-	this.activate(ix);
+    this.activate(ix);
 
 }
--- test/all.js
+++ test/all.js
@@ -4,135 +4,135 @@ var test = require('tape');
 function Item() {};
 
 function createManager() {
-	var is = [new Item, new Item, new Item];
-	var m = new Manager();
-	is.forEach(function(i) { m.add(i); });
-	return {manager: m, items: is};
+    var is = [new Item, new Item, new Item];
+    var m = new Manager();
+    is.forEach(function(i) { m.add(i); });
+    return {manager: m, items: is};
 }
 
 test("adding item emits change:add event", function(assert) {
 
-	var m = new Manager();
-	
-	m.events.on("change:add", function(i) {
-		assert.equal(i, item);
-	});
+    var m = new Manager();
+    
+    m.events.on("change:add", function(i) {
+        assert.equal(i, item);
+    });
 
-	var item = new Item();
-	m.add(item);
+    var item = new Item();
+    m.add(item);
 
-	assert.end();
+    assert.end();
 
 });
 
 test("adding first item emits activate after change:add", function(assert) {
 
-	var m = new Manager();
+    var m = new Manager();
 
-	var state = 0;
+    var state = 0;
 
-	m.events.on("change:add", function(i) {
-		assert.equal(state++, 0);
-	});
+    m.events.on("change:add", function(i) {
+        assert.equal(state++, 0);
+    });
 
-	m.events.on("activate", function(newItem, oldItem) {
-		assert.equal(state++, 1);
-		assert.equal(newItem, item);
-		assert.equal(oldItem, null);
-		assert.equal(m.active(), newItem);
-		assert.end();
-	});
+    m.events.on("activate", function(newItem, oldItem) {
+        assert.equal(state++, 1);
+        assert.equal(newItem, item);
+        assert.equal(oldItem, null);
+        assert.equal(m.active(), newItem);
+        assert.end();
+    });
 
-	var item = new Item();
-	m.add(item);
+    var item = new Item();
+    m.add(item);
 
 });
 
 test("adding second item does not emit activate event", function(assert) {
 
-	var m = new Manager();
+    var m = new Manager();
 
-	m.add(new Item());
+    m.add(new Item());
 
-	m.events.on("activate", function(active) {
-		assert.ok(false);
-	});
+    m.events.on("activate", function(active) {
+        assert.ok(false);
+    });
 
-	m.add(new Item());
+    m.add(new Item());
 
-	assert.end();
+    assert.end();
 
 });
 
 test("activating item by index", function(assert) {
 
-	var items = [new Item, new Item, new Item];
-	var m = new Manager();
-	items.forEach(function(i) { m.add(i); });
+    var items = [new Item, new Item, new Item];
+    var m = new Manager();
+    items.forEach(function(i) { m.add(i); });
 
-	m.activate(1);
+    m.activate(1);
 
-	assert.equal(m.active(), items[1]);
-	assert.end();
+    assert.equal(m.active(), items[1]);
+    assert.end();
 
 });
 
 test("activating item by object", function(assert) {
 
-	var t = createManager();
-	
-	t.manager.activate(t.items[2]);
+    var t = createManager();
+    
+    t.manager.activate(t.items[2]);
 
-	assert.equal(t.manager.active(), t.items[2]);
-	assert.end();
+    assert.equal(t.manager.active(), t.items[2]);
+    assert.end();
 
 });
 
 test("activating item emits event", function(assert) {
 
-	var t = createManager();
+    var t = createManager();
 
-	t.manager.events.on('activate', function(newItem, oldItem) {
-		assert.equal(newItem, t.manager.active());
-		assert.equal(oldItem, t.manager.at(0));
-		assert.end();
-	});
+    t.manager.events.on('activate', function(newItem, oldItem) {
+        assert.equal(newItem, t.manager.active());
+        assert.equal(oldItem, t.manager.at(0));
+        assert.end();
+    });
 
-	t.manager.activate(t.items[2]);
-	
+    t.manager.activate(t.items[2]);
+    
 });
 
 test("previous", function(assert) {
 
-	var t = createManager();
+    var t = createManager();
 
-	t.manager.previous();
-	assert.equals(t.manager.active(), t.items[2]);
+    t.manager.previous();
+    assert.equals(t.manager.active(), t.items[2]);
 
-	t.manager.previous();
-	assert.equals(t.manager.active(), t.items[1]);
+    t.manager.previous();
+    assert.equals(t.manager.active(), t.items[1]);
 
-	t.manager.previous();
-	assert.equals(t.manager.active(), t.items[0]);
+    t.manager.previous();
+    assert.equals(t.manager.active(), t.items[0]);
 
-	assert.end();
+    assert.end();
 
 });
 
 test("next", function(assert) {
 
-	var t = createManager();
+    var t = createManager();
 
-	t.manager.next();
-	assert.equals(t.manager.active(), t.items[1]);
+    t.manager.next();
+    assert.equals(t.manager.active(), t.items[1]);
 
-	t.manager.next();
-	assert.equals(t.manager.active(), t.items[2]);
+    t.manager.next();
+    assert.equals(t.manager.active(), t.items[2]);
 
-	t.manager.next();
-	assert.equals(t.manager.active(), t.items[0]);
+    t.manager.next();
+    assert.equals(t.manager.active(), t.items[0]);
 
-	assert.end();
+    assert.end();
 
 });
 
